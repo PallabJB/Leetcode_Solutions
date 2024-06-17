fda8f1a4d217bac8fe3314e0bf054dd677f5e707
Pallab Jyoti Bora <111581851+PallabJB@users.noreply.github.com>
2024-06-17 12:11:34 +0530

    633. Sum of Square Numbers.java

---
+++ 633. Sum of Square Numbers.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long left=0, right=(long)Math.sqrt(c);       
+        while(left<=right){
+            long num = left*left + right*right;
+            if(num==c) return true;
+            if(num>c) right -=1;
+            else left +=1;
+        }
+        return false;
+    }
+}
