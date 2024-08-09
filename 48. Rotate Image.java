39c569890323fc4785eceeff74d148bbc43bee5e
Pallab Jyoti Bora <111581851+PallabJB@users.noreply.github.com>
2024-08-10 03:25:43 +0530

    48. Rotate Image.java

---
+++ 48. Rotate Image.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for(int i=0; i<(n+1)/2; i++){
+            for(int j=0; j<n/2;j++){
+                //starting the 4 way swao
+                // assigning temp = bottom left
+                int temp = matrix[n-1-j][i];
+                //bottom left = bottom right
+                matrix[n-1-j][i] = matrix[n-1-i][n-j-1];
+                //bottom right =  top  right
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                //top right = top left
+                matrix[j][n-1-i] = matrix[i][j];
+                //top left = temp
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
